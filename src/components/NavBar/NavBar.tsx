d86f9df854d3e012f52cb23b10622f18ec9d2519
SiriJV <siri.viren@icloud.com>
2025-04-22 17:09:25 +0200

    Added active styling to NavLinks in NavBar

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './NavBar.scss'
 
 const NavBar = () => {
     return (
         <nav>
-            <Link to="/home">Home</Link>
-            <Link to="/explore">Explore</Link>
-            <Link to="/bookshelves">Bookshelves</Link>
-            <Link to="/profile">Profile</Link>
+            <NavLink to="/home" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Home</NavLink>
+            <NavLink to="/explore" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Explore</NavLink>
+            <NavLink to="/bookshelves" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Bookshelves</NavLink>
+            <NavLink to="/profile" className={({ isActive }) => (isActive ? "nav-link active" : "nav-link")}>Profile</NavLink>
+
         </nav>
     )
 }
