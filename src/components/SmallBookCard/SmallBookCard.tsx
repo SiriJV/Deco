469fba0d0397f958302de0823833f0e579175785
SiriJV <siri.viren@icloud.com>
2025-05-05 10:42:29 +0200

    Added SmallBookCard

---
+++ src/components/SmallBookCard/SmallBookCard.tsx
@@ -0,0 +1,22 @@
+import "./SmallBookCard.scss";
+
+type SmallBookCardProps = {
+  img: string;
+  name: string;
+};
+
+function BookCard({ img, name }: SmallBookCardProps) {
+  return (
+    <div className="small-book-card">
+      <img src={img} alt={name} className="small-book-card-image" />
+      <div className="small-book-card-info">
+        <h3 className="small-book-card-name">{name}</h3>
+      </div>
+      <button className="delete-icon">
+        delete
+      </button>
+    </div>
+  );
+}
+
+export default BookCard;
