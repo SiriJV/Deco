94d7b681c5c077b660ed113e701683f04a184e36
SiriJV <siri.viren@icloud.com>
2025-05-05 10:42:09 +0200

    Added context to my books page

--- src/pages/MyBooks.tsx
+++ src/pages/MyBooks.tsx
@@ -1,7 +1,41 @@
-const MyBooks = () => {
-    return (
-        <h1>My books</h1>
-    )
-}
+import { useState } from "react";
+import BookCard from "../components/BookCard/BookCard";
+import { useShelves } from "../context/ShelvesContext";
+import ShelfForm from "../components/ShelfForm/ShelfForm";
 
-export default MyBooks
+import './MyBooks.scss'
+import ShelfCard from "../components/ShelfCard/ShelfCard";
+
+const MyBooks = () => {
+  const { shelves, removeShelf, editShelf, addShelf } = useShelves();
+  const [showShelfForm, setShowShelfForm] = useState(false);
+
+  const handleRemoveShelf = (shelfName: string) => {
+    removeShelf(shelfName);
+  };
+
+  const handleAddNewShelf = () => {
+    setShowShelfForm(true);
+  };
+
+  return (
+    <article className="mybooks-page">
+        <div className="main-content">
+    <button onClick={handleAddNewShelf} className="add-shelf-button">
+        Add New Shelf
+    </button>
+    <div className="shelves">
+        {shelves.map((shelf) => (
+        <ShelfCard key={shelf.name} name={shelf.name} books={shelf.books} />
+    ))}
+
+    {showShelfForm && (
+        <ShelfForm onClose={() => setShowShelfForm(false)} />
+    )}
+    </div>
+    </div>
+    </article>
+  );
+};
+
+export default MyBooks;
