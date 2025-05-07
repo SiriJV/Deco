36cfc01bde14cd6ee9109496976fbe850b34aad3
SiriJV <siri.viren@icloud.com>
2025-05-07 22:58:45 +0200

    Fixed routing in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,117 +1,58 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './App.scss'
-import Home from './pages/Home'
-import Explore from './pages/Explore'
-import MyBooks from './pages/MyBooks'
-import Profile from './pages/Profile'
+import './App.scss';
+import Home from './pages/Home';
+import Explore from './pages/Explore';
+import MyBooks from './pages/MyBooks';
+import Profile from './pages/Profile';
 import RootLayout from './layouts/RootLayout';
-import BookModal from './components/BookModal/BookModal'
+import { ShelvesProvider } from './context/ShelvesContext';
+import BookModal from './components/BookModal/BookModal';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
     children: [
-      { index: true, element: <Home />},
       {
-        path: 'mybooks',
-        element: <MyBooks />,
+        path: 'home',
+        // element: <Home />,
+        children: [
+          {
+            index: true,
+            element: <Home /> // renders the search
+          },
+          {
+            path: ':bookId',
+            element: <BookModal />
+          }
+        ]
       },
       {
         path: 'explore',
         element: <Explore />,
+        // children: [
+        //   {
+        //     path: ':bookId',
+        //     element: <BookModal />
+        //   }
+        // ]
+      },
+      {
+        path: 'mybooks',
+        element: <MyBooks />
       },
       {
         path: 'profile',
-        element: <Profile />,
-        children: [
-          {
-            path: ':bookid',
-            element: <BookModal />,
-          },
-        ],
+        element: <Profile />
       },
-    ],
+    ]
   },
 ]);
 
 export default function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <ShelvesProvider>
+      <RouterProvider router={router} />
+    </ShelvesProvider>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-// function App() {
-//   return (
-//     <>
-//     <BrowserRouter>
-//       <NavBar />
-//       <main>
-//       <Routes>
-//         <Route path="/" element={<Navigate to="/home" replace />} />
-//         <Route path="/home" element={<Home />} />
-//         <Route path="/explore" element={<Explore />} />
-//         <Route path="/mybooks" element={<MyBooks />} />
-//         <Route path="/profile" element={<Profile />} />
-//       </Routes>
-//       </main>
-//     </BrowserRouter>
-//     </>
-//   )
-// }
-
-// export default App
-
-
-
-
-
-
-
-
-
-
-
-
-{/* <section className="temporary-search-wrapper">
-        <SearchBar />
-        <FilterButton />
-      </section>
-        <h2 className="temporary-title">Developers Favs</h2>
-      <section className="temporary-bookcard-wrapper">
-      <BookCard
-        img="public\images\fifteen_dogs.jpg"
-        name="Fifteen Dogs"
-        author="André Alexis"
-      />
-      <BookCard
-        img="public\images\strange_the_dreamer.jpg"
-        name="Strange the Dreamer"
-        author="Laini Taylor"
-      />
-      <BookCard
-        img="public\images\bunny.jpg"
-        name="Bunny"
-        author="Mona Awad"
-      />
-      <BookCard
-        img="public\images\mockingjay.jpeg"
-        name="Mockingjay"
-        author="Suzanne Collins"
-      />
-      <BookCard
-        img="public\images\alls_well.jpg"
-        name="All's Well"
-        author="Mona Awad"
-      />
-      </section>
-        <Rating /> */}
