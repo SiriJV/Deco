655830517c9992a4fe640b4a35a838cd49ce7bbe
SiriJV <siri.viren@icloud.com>
2025-05-07 22:57:59 +0200

    Added working search

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+import SearchBar from "../components/SearchBar/SearchBar"
+import SearchResult from "../components/SearchResult/SearchResult";
+import { Outlet } from "react-router-dom";
+import './Home.scss'
+
 const Home = () => {
+    const [books, setBooks] = useState([]);
+    const [loading, setLoading] = useState(false);
+  
+    const handleSearch = async (query: string, filter: string) => {
+        setLoading(true);
+        try {
+          const response = await fetch(`https://openlibrary.org/search.json?${filter}=${encodeURIComponent(query)}`);
+          const data = await response.json();
+          setBooks(data.docs.slice(0, 20));
+        } catch (error) {
+          console.error("Error fetching books:", error);
+        } finally {
+          setLoading(false);
+        }
+    };
+      
     return (
-        <>
-        <h1>Home</h1>
-        {/* <NavBar /> */}
-        </>
+        <section className="home-page">
+        <SearchBar onSearch={handleSearch}/>
+        <SearchResult books={books} loading={loading} />
+        <Outlet />
+        </section>
     )
 }
 
