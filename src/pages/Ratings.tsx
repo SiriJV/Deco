8aab0663d2f287eea499cc898e9e970a85cea1ca
SiriJV <siri.viren@icloud.com>
2025-05-07 22:48:13 +0200

    Added Ratings page

---
+++ src/pages/Ratings.tsx
@@ -0,0 +1,13 @@
+import './Ratings.scss';
+
+const Ratings = () => {
+  return (
+    <article className="ratings-page">
+        <div className="ratings-main-content">
+            
+        </div>
+    </article>
+  );
+};
+
+export default Ratings;
