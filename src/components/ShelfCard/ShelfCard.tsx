9adc5550cf389a0c935496935721057cad2c666f
SiriJV <siri.viren@icloud.com>
2025-05-15 08:53:31 +0200

    Fix removeBookFromShelf function

--- src/components/ShelfCard/ShelfCard.tsx
+++ src/components/ShelfCard/ShelfCard.tsx
@@ -9,19 +9,19 @@ import { useNavigate } from 'react-router-dom';
 
 type ShelfCardProps = {
   name: string;
-  books: Book[];
 };
 
-const ShelfCard: React.FC<ShelfCardProps> = ({ name, books }) => {
-  const { removeShelf, editShelf, addBookToShelf } = useShelves();
+const ShelfCard = ({ name }: ShelfCardProps) => {
+  const { shelves, removeShelf, editShelf, removeBookFromShelf } = useShelves();
   const [editing, setEditing] = useState(false);
   const [newName, setNewName] = useState(name);
-  const [shelfBooks, setShelfBooks] = useState<Book[]>(books);
+
+  const shelf = shelves.find((shelf) => shelf.name === name);
+  const shelfBooks = shelf ? shelf.books : [];
 
   const handleRemoveBook = (index: number) => {
-    const updatedBooks = [...shelfBooks];
-    updatedBooks.splice(index, 1);
-    setShelfBooks(updatedBooks);
+    const bookToRemove = shelfBooks[index];
+    removeBookFromShelf(name, bookToRemove);
   };
 
   const handleSave = () => {
@@ -32,20 +32,20 @@ const ShelfCard: React.FC<ShelfCardProps> = ({ name, books }) => {
   const handleCancel = () => {
     setEditing(false);
     setNewName(name);
-    setShelfBooks(books);
+    // setShelfBooks(books);
   };
 
   const navigate = useNavigate();
     
     const handleExploreBooks = () => {
-      navigate("/explore");
+      navigate("/home");
     };
 
   return (
     <div className="shelf">
       {editing ? (
         <section className="shelf-card-header-editing">
-          <input value={newName} onChange={(e) => setNewName(e.target.value)}/>
+          <input className="shelf-card-editing-input" value={newName} onChange={(e) => setNewName(e.target.value)}/>
           <Button variant="default" onClick={() => removeShelf(name)}>
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash2-icon lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
           </Button>
@@ -53,7 +53,7 @@ const ShelfCard: React.FC<ShelfCardProps> = ({ name, books }) => {
       ) : (
         <>
         <div className="shelf-card-header">
-            <h3>{name} ({books.length})</h3>
+            <h3>{name} ({shelfBooks.length})</h3>
             <Button variant="default" onClick={() => setEditing(true)}>
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-pen-line-icon lucide-pen-line"><path d="M12 20h9"/><path d="M16.376 3.622a1 1 0 0 1 3.002 3.002L7.368 18.635a2 2 0 0 1-.855.506l-2.872.838a.5.5 0 0 1-.62-.62l.838-2.872a2 2 0 0 1 .506-.854z"/></svg>
             </Button>
@@ -69,7 +69,9 @@ const ShelfCard: React.FC<ShelfCardProps> = ({ name, books }) => {
             <div key={index}>
               <SmallBookCard {...book} />
               {editing && (
-                <button onClick={() => handleRemoveBook(index)}>Remove</button>
+                <Button variant="link" className="remove-book-from-shelf-button-2" onClick={() => handleRemoveBook(index)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-trash2-icon lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
+                </Button>
               )}
             </div>
           ))
