c966fd2fa73509a8c00a5e6096cabc25a2c2bed2
SiriJV <siri.viren@icloud.com>
2025-05-15 02:18:18 +0200

    Added data for BookCarousel

---
+++ src/data/genreBooks.tsx
@@ -0,0 +1,152 @@
+import { Book } from "../components/BookCarousel/BookCarousel";
+
+export const favouriteBooks: Book[] = [
+    {
+      key: "OL17719038W",
+      title: "Strange the Dreamer",
+      author_name: ["Laini Taylor"],
+      cover_i: 8597976,
+    },
+    {
+      key: "OL17716842W",
+      title: "Fifteen Dogs",
+      author_name: ["André Alexis"],
+      cover_i: 8462871,
+    },
+    {
+      key: "OL20161633W",
+      title: "Bunny",
+      author_name: ["Mona Awad"],
+      cover_i: 8807371,
+    },
+    {
+      key: "OL15413843W",
+      title: "Mockingjay",
+      author_name: ["Suzanne Collins"],
+      cover_i: 12646459,
+    },
+    {
+      key: "OL24205364W",
+      title: "All's Well",
+      author_name: ["Mona Awad"],
+      cover_i: 10648818,
+    },
+    {
+      key: "OL20883297W",
+      title: "Klara and the Sun",
+      author_name: ["Kazuo Ishiguro"],
+      cover_i: 14579150,
+    },
+    {
+      key: "OL19708587W",
+      title: "The concept of anxiety",
+      author_name: ["Søren Kierkegaard"],
+      cover_i: 12196222,
+    },
+    {
+      key: "OL20150260W",
+      title: "Normal People",
+      author_name: ["Sally Rooney"],
+      cover_i: 8794265,
+    },
+    {
+      key: "OL26004554W",
+      title: "Tomorrow, and Tomorrow, and Tomorrow",
+      author_name: ["Gabrielle Zevin"],
+      cover_i: 12859975,
+    },
+    {
+      key: "OL15030850W",
+      title: "Holes",
+      author_name: ["Louis Sachar"],
+      cover_i: 234567,
+    },
+    {
+      key: "OL14973701W",
+      title: "Killer",
+      author_name: ["Sara Shepard"],
+      cover_i: 6304259,
+    },
+    {
+      key: "OL66513W",
+      title: "Emma",
+      author_name: ["Jane Austen"],
+      cover_i: 9278312,
+    },
+];
+
+export const scienceFictionBooks: Book[] = [
+    {
+      key: "OL893415W",
+      title: "Dune",
+      author_name: ["Frank Herbert"],
+      cover_i: 11481354,
+    },
+    {
+      key: "OL27258W",
+      title: "Neuromancer",
+      author_name: ["William Gibson"],
+      cover_i: 283860,
+    },
+    {
+      key: "OL46125W",
+      title: "Foundation",
+      author_name: ["Isaac Asimov"],
+      cover_i: 14612610,
+    },
+    {
+      key: "OL38501W",
+      title: "Snow Crash",
+      author_name: ["Neal Stephenson"],
+      cover_i: 392508,
+    },
+    {
+      key: "OL59800W",
+      title: "The Left Hand of Darkness",
+      author_name: ["Ursula K. Le Guin"],
+      cover_i: 10618463,
+    },
+    {
+      key: "OL49488W",
+      title: "Ender's Game",
+      author_name: ["Orson Scott Card"],
+      cover_i: 8231856,
+    },
+    {
+      key: "OL1963268W",
+      title: "Hyperion",
+      author_name: ["Dan Simmons"],
+      cover_i: 380332,
+    },
+    {
+      key: "OL17091839W",
+      title: "The Martian",
+      author_name: ["Andy Weir"],
+      cover_i: 11447888,
+    },
+    {
+      key: "OL64365W",
+      title: "Brave New World",
+      author_name: ["Aldous Huxley"],
+      cover_i: 8231823,
+    },
+    {
+      key: "OL2172356W",
+      title: "Do Androids Dream of Electric Sheep?",
+      author_name: ["Philip K. Dick"],
+      cover_i: 207515,
+    },
+    {
+      key: "OL52267W",
+      title: "The Time Machine",
+      author_name: ["H.G. Wells"],
+      cover_i: 9009316,
+    },
+    {
+      key: "OL46241W",
+      title: "I, Robot",
+      author_name: ["Isaac Asimov"],
+      cover_i: 12385229,
+    },
+  ];
+  
