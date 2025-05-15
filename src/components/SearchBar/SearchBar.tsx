ab79c1505668607a209689eee376cbc33cd625b7
SiriJV <siri.viren@icloud.com>
2025-05-15 08:27:39 +0200

    Small changes

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -19,14 +19,16 @@ function SearchBar({ onSearch }: SearchBarProps) {
 
   return (
     <div className="search-bar">
-      <form onSubmit={handleSubmit}>
+      <form className="search-wrapper" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="Search for books..."
           value={query}
           onChange={(e) => setQuery(e.target.value)}
         />
-        <button type="submit">Search</button>
+        <button type="submit" className="search-button">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="lucide lucide-search-icon lucide-search"><path d="m21 21-4.34-4.34"/><circle cx="11" cy="11" r="8"/></svg>
+        </button>
       </form>
 
       {query && (
