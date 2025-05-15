039e75834696dca861646cae3bac0a6d3f1b0467
SiriJV <siri.viren@icloud.com>
2025-05-15 02:15:49 +0200

    Added link to see more books in genre

--- src/components/BookCarousel/BookCarousel.tsx
+++ src/components/BookCarousel/BookCarousel.tsx
@@ -2,63 +2,73 @@ import { useEffect, useRef, useState } from "react";
 import BookCard from "../BookCard/BookCard";
 import './BookCarousel.scss'
 import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
 
 export type Book = {
   key: string;
   title: string;
   author_name?: string[];
   cover_i?: number;
+  numberOfPages?: number;
 };
 
 type BookCarouselProps = {
   title: string;
   books?: Book[];
+  genre: string;
 };
 
-const BookCarousel = ({ title, books }: BookCarouselProps) => {
-  const [carouselBooks, setCarouselBooks] = useState<Book[]>([]);
-  const scrollRef = useRef<HTMLDivElement>(null);
+const BookCarousel = ({ title, books, genre }: BookCarouselProps) => {
+    const [carouselBooks, setCarouselBooks] = useState<Book[]>([]);
+    // const scrollRef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (books) {
-      setCarouselBooks(books);
-    } else {
-      fetch("https://openlibrary.org/subjects/love.json?limit=20")
-        .then((res) => res.json())
-        .then((data) => setCarouselBooks(data.works))
-        .catch((err) => console.error("Fetch failed:", err));
-    }
-  }, [books]);
+    const navigate = useNavigate();
 
-  return (
-    <div className="book-carousel">
-      <div className="carousel-header">
-        <h2>{title}</h2>
-        <Button variant="link" onClick={() => alert(`se alla böcker i "${title}"`)}>See more</Button>
-      </div>
+    const handleSeeMore = () => {
+      navigate(`/home?filter=subject&query=${encodeURIComponent(genre)}`);
+    };
 
-      <div className="scroll-area">
-        {/* <button onClick={() => scroll("left")}>left</button> */}
+    useEffect(() => {
+      if (books) {
+        setCarouselBooks(books);
+      } else {
+        fetch("https://openlibrary.org/subjects/love.json?limit=20")
+          .then((res) => res.json())
+          .then((data) => setCarouselBooks(data.works))
+          .catch((err) => console.error("Fetch failed:", err));
+      }
+    }, [books]);
 
-        <div className="carousel-content">
-          {carouselBooks.map((book) => (
-            <BookCard
-              key={book.key}
-              name={book.title}
-              author={book.author_name?.[0] || "Unknown"}
-              img={
-                book.cover_i
-                  ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
-                  : "/placeholder.jpg"
-              }
-            />
-          ))}
+    return (
+      <div className="book-carousel">
+        <div className="carousel-header">
+          <h2>{title}</h2>
+          <Button variant="link" onClick={handleSeeMore}>See more</Button>
         </div>
 
-        {/* <button onClick={() => scroll("right")}>right</button> */}
+        <div className="scroll-area">
+          {/* <button onClick={() => scroll("left")}>left</button> */}
+
+          <div className="carousel-content">
+              {carouselBooks.map((book) => (
+              <BookCard
+                  id={book.key}
+                  name={book.title}
+                  author={book.author_name?.[0] || "Unknown"}
+                  img={
+                      book.cover_i
+                          ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+                          : "/placeholder.jpg"
+                      }
+                  numberOfPages={book.numberOfPages}
+                  />
+              ))}
+          </div>
+
+          {/* <button onClick={() => scroll("right")}>right</button> */}
+        </div>
       </div>
-    </div>
-  );
+    );
 };
 
 export default BookCarousel;
