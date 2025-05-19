b9d79f05c9068665d01dd1198b13086abb1df741
SiriJV <siri.viren@icloud.com>
2025-05-19 10:49:59 +0200

    Replace placeholder icon with star

--- src/components/BookModal/Rating/Rating.tsx
+++ src/components/BookModal/Rating/Rating.tsx
@@ -11,7 +11,6 @@ const Rating = ({ title }: { title: string }) => {
   const [isLocked, setIsLocked] = useState(false);
   const [showModal, setShowModal] = useState(false);
 
-
   useEffect(() => {
     if (bookId && ratings[bookId]) {
       setRating(ratings[bookId].rating);
@@ -28,7 +27,7 @@ const Rating = ({ title }: { title: string }) => {
     }
   };
 
-  const handleChange = (newRating: number) => {
+  const handleChangeRating = (newRating: number) => {
     if (bookId) {
       setRating(newRating);
       saveRating(bookId, title, newRating);
@@ -40,13 +39,24 @@ const Rating = ({ title }: { title: string }) => {
     <div className="rating-wrapper">
       <div className="rating-icons">
         {[...Array(5)].map((_, i) => (
-          <div
-            key={i}
-            className={`shape ${i < rating ? 'filled' : ''}`}
-            onClick={() => handleClick(i)}
-          />
+          <div key={i} onClick={() => handleClick(i)}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill={i < rating ? "currentColor" : "none"}
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
+            </svg>
+          </div>
         ))}
       </div>
+
 
       {isLocked && (
         <div className="rating-result">
@@ -63,7 +73,7 @@ const Rating = ({ title }: { title: string }) => {
               <div
                 key={i}
                 className={`shape ${i < rating ? 'filled' : ''}`}
-                onClick={() => handleChange(i + 1)}
+                onClick={() => handleChangeRating(i + 1)}
               />
             ))}
           </div>
