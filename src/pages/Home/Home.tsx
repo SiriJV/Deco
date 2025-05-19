410724d752f2c994e54da2f1adc555b51d491c4d
SiriJV <siri.viren@icloud.com>
2025-05-19 10:50:31 +0200

    Fix searchparams

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar"
 import SearchResult from "../../components/SearchResult/SearchResult";
-import { Outlet, useSearchParams } from "react-router-dom";
+import { Outlet, useNavigate, useSearchParams } from "react-router-dom";
 import './Home.scss'
 import BookCarousel from "../../components/BookCarousel/BookCarousel";
 import { favouriteBooks } from "../../data/genreBooks";
@@ -11,7 +11,18 @@ const Home = () => {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(false);
     const [searchParams] = useSearchParams();
+    const navigate = useNavigate();
   
+    useEffect(() => {
+        const query = searchParams.get("query");
+        const filter = searchParams.get("filter");
+        if (query && filter) {
+          handleSearch(query, filter);
+        }
+      }, [searchParams]);
+
+    const query = searchParams.get("query");
+
     const handleSearch = async (query: string, filter: string) => {
         setLoading(true);
         try {
@@ -24,36 +35,26 @@ const Home = () => {
           setLoading(false);
         }
     };
-
-    useEffect(() => {
-        const query = searchParams.get("query");
-        const filter = searchParams.get("filter");
-        if (query && filter) {
-          handleSearch(query, filter);
-        }
-      }, [searchParams]);
-
-      const query = searchParams.get("query");
       
     return (
         <section className="home-page">
-        <SearchBar onSearch={handleSearch}/>
-        {books.length > 0 && query && (
-            <p className="search-info">Showing search results for "{query}"</p>
-        )}
-        <SearchResult books={books} loading={loading} />
-        {!loading && books.length === 0 && (
-        <div className="no-results">
-            <div className="book-carousel-wrapper">
-                <BookCarousel title="Developers’ favourites" books={favouriteBooks} genre="science fiction"/>
-                <BookCarousel title="Science fiction" books={scienceFictionBooks} genre="science fiction"/>
-                <BookCarousel title="Developers’ favourites" books={favouriteBooks} genre="science fiction"/>
-                <BookCarousel title="Science fiction" books={scienceFictionBooks} genre="science fiction"/>
-            </div>
-        </div>
-)}
-        <Outlet />
-        <div className="search-filler"></div>
+          <SearchBar />
+          {books.length > 0 && query && (
+              <p className="search-info">Showing search results for "{query}"</p>
+          )}
+          <SearchResult books={books} loading={loading} />
+          {!loading && books.length === 0 && (
+          <div className="no-results">
+              <div className="book-carousel-wrapper">
+                  <BookCarousel title="Developers’ favourites" books={favouriteBooks} genre="science fiction"/>
+                  <BookCarousel title="Science fiction" books={scienceFictionBooks} genre="science fiction"/>
+                  <BookCarousel title="Developers’ favourites" books={favouriteBooks} genre="science fiction"/>
+                  <BookCarousel title="Science fiction" books={scienceFictionBooks} genre="science fiction"/>
+              </div>
+          </div>
+          )}
+          <Outlet />
+          <div className="search-filler"></div>
         </section>
     )
 }
